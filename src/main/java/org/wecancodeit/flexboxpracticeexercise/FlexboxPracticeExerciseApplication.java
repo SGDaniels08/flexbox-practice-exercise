e8ca4c72b64142a3219e695d06a67e7d39cc6408
Don Hamilton III <donhamiltoniii@gmail.com>
2018-10-04 14:49:10 -0400

    Add Spring Boot backend

---
+++ src/main/java/org/wecancodeit/flexboxpracticeexercise/FlexboxPracticeExerciseApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.flexboxpracticeexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexboxPracticeExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexboxPracticeExerciseApplication.class, args);
+	}
+}
